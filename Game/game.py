8853b88b5af16da6363db8962faea25167d65335
Caelan Winmill <calnwins672@gmail.com>
2024-07-17 18:39:24 -0400

    Update

---
+++ Game/game.py
@@ -0,0 +1,11 @@
+import pygame
+import events
+
+class Game(events.EventAcceptor):
+    def __init__(self):
+
+    def load_bgs(self):
+        pass
+
+    def draw_bg(self, surface):
+        surface.fill((192, 0, 0))
