29bc51b2972cc2b791f8bbfbe2c4066df93317b0
Caelan Winmill <calnwins672@gmail.com>
2024-07-24 23:27:33 -0400

    Improved tiles

--- Game/game.py
+++ Game/game.py
@@ -5,12 +5,7 @@ import os
 import events
 from textures import TextureAtlas
 
-TILE_SIZE = 32
 ASSETS = os.path.join("assets", "game")
-
-class TEXTURES:
-    TILES = ["barrier",
-             "grass"]
 
 class GameManager(events.Alpha):
     def __init__(self, screen_size):
@@ -47,8 +42,9 @@ class GameManager(events.Alpha):
     def start(self):
         print("Game starting!")
         
-    def onKeyDown(self, key, mod, unicode):
-        pass
+    def onMouseDown(self, pos, button):
+        adj_pos = [p+d for p, d in zip(pos, self.player_pos)]
+        self.world.onMouseDown(adj_pos, button)
 
     def close(self):
         print("Game closing!")
@@ -64,25 +60,126 @@ class GameManager(events.Alpha):
     def drawWorld(self, surface):
         delta = [-coord for coord in self.player_pos]
         self.world.draw(surface, delta)
+
+
+class BasicTile:
+    def __init__(self, tileid, tex_name, tile_list):
+        self.tileid = tileid
+        self.tex_name = tex_name
+
+        tile_list.append(self)
+
+        self._atlas_given = False
+
+    def setAtlas(self, atlas):
+        self.atlas = atlas
+
+        self.tex_loc = self.atlas.getTextureLoc(self.tex_name)
+
+        self._atlas_given = True
+
+    def getTileID(self):
+        return self.tileid
+
+    def onLeft(self, world, tile_pos): pass
+    def onRight(self, world, tile_pos): pass
+    def onWalk(self, world, tile_pos): pass
+
+    def draw(self, surface, pos):
+        if self._atlas_given:
+            self.atlas.drawTextureAtLoc(surface, pos, self.tex_loc)
+
+class GrassTile(BasicTile):
+    def __init__(self, tile_list):
+        super().__init__("grass", "grass", tile_list)
+
+    def onLeft(self, world, tile_pos):
+        world.setTileID(tile_pos, "barrier")
+
+class BarrierTile(BasicTile):
+    def __init__(self, tile_list):
+        super().__init__("barrier", "barrier", tile_list)
+
+    def onRight(self, world, tile_pos):
+        world.setTileID(tile_pos, "grass")
+
+OVERWORLD_TILES = []
+GrassTile(OVERWORLD_TILES)
+BarrierTile(OVERWORLD_TILES)
+BasicTile("gremlin",
+          "barrier",
+          OVERWORLD_TILES)
+    
         
-class World:
+class World(events.EventAcceptor):
+    class MAP_TILES_TEXTURE_PRESETS:
+        FOLDER  = os.path.join(ASSETS, "tiles")
+        SIZE    = 32
+        EXT     = ".png"
+
+    ALL_TILES = OVERWORLD_TILES
+
+    
     def __init__(self):
         self.loadWorld()
 
-        self.buffer = pygame.Surface((self.world_data.shape[0]*TILE_SIZE,
-                                      self.world_data.shape[1]*TILE_SIZE))
+        self.buffer = pygame.Surface((3200, 3200))
 
-        self.tileAtlas = TextureAtlas(os.path.join(ASSETS, "tiles"), ".png", (32, 32), TEXTURES.TILES)
+        self.tileAtlas = TextureAtlas.fromPreset(self.MAP_TILES_TEXTURE_PRESETS)
 
-        self.drawMapToBuffer()
+        self.tiles = {}
+        for tileClass in self.ALL_TILES:
+            tileClass.setAtlas(self.tileAtlas)
+            self.tiles[tileClass.getTileID()] = tileClass
+
+        self.updateMapToBuffer()
 
     def loadWorld(self):
-        self.world_data = np.array(np.random.random_sample(10000).reshape((100, 100))*2, dtype=np.int16)
+        self.world_data = np.array(["grass", "grass"]*5000, dtype='<U16').reshape((100, 100))
 
-    def drawMapToBuffer(self):
+    def getTileID(self, tile_pos):
+        if (tile_pos[0] < 0 or tile_pos[1] < 0):
+            return None
+        
+        try:
+            return self.world_data[tile_pos[1]][tile_pos[0]]
+        except IndexError:
+            return None
+
+    def getTile(self, tile_pos):
+        return self.tiles[self.getTileID(tile_pos)]
+
+    def setTileID(self, tile_pos, tile_id):
+        if (tile_pos[0] < 0 or tile_pos[1] < 0):
+            return None
+        
+        if not tile_id in self.tiles.keys():
+            return None
+
+        try:
+            self.world_data[tile_pos[1]][tile_pos[0]] = tile_id
+            self.updateTileToBuffer(tile_pos)
+        except IndexError:
+            return None
+        
+    def onMouseDown(self, pos, button):
+        tile_size = self.tileAtlas.getTextureSize()
+        tile_pos = (*(p//t for p, t in zip(pos, tile_size)),)
+
+        if button == pygame.BUTTON_LEFT:
+            self.getTile(tile_pos).onLeft(self, tile_pos)
+
+        elif button == pygame.BUTTON_RIGHT:
+            self.getTile(tile_pos).onRight(self, tile_pos)
+
+    def updateTileToBuffer(self, tile_pos):
+        tileid = self.world_data[tile_pos[1]][tile_pos[0]]
+        self.tiles[tileid].draw(self.buffer, (tile_pos[0]*self.tileAtlas.getTextureWidth(), tile_pos[1]*self.tileAtlas.getTextureHeight()))
+        
+    def updateMapToBuffer(self):
         for i in range(self.world_data.shape[0]): #y
             for j in range(self.world_data.shape[1]): #x
-                self.tileAtlas.drawTextureAtLoc(self.buffer, (j*TILE_SIZE, i*TILE_SIZE), self.world_data[i][j])
+                self.updateTileToBuffer((j, i))
 
     def draw(self, surface, delta):
         surface.blit(self.buffer, delta)
--- Game/textures.py
+++ Game/textures.py
@@ -1,17 +1,32 @@
 import pygame
 import os
 
+
+def getAllXFilesInFolder(folder, ext):
+    files = os.listdir(folder)
+    for file in files:
+        if file.endswith(ext):
+            yield file.rstrip(ext)
+
+
 class TextureAtlas:
     def __init__(self, path, ext, texture_size, asset_list, rescale=False):
         self.path = path
         self.ext = ext
         self.size = texture_size
-        self.asset_list = asset_list
-        self.rescale = False
+        self.asset_list = tuple(asset_list)
+        self.rescale = rescale
 
         self.default = pygame.Surface(texture_size)
 
         self.createAtlas()
+
+    @classmethod
+    def fromPreset(cls, preset):
+        return cls(preset.FOLDER,
+                   preset.EXT,
+                   (preset.SIZE, preset.SIZE),
+                   getAllXFilesInFolder(preset.FOLDER, preset.EXT))
 
     def createAtlas(self):
         self.atlas = pygame.Surface((self.size[0]*len(self.asset_list), self.size[1]))
@@ -31,18 +46,41 @@ class TextureAtlas:
     def getAtlas(self):
         return self.atlas
 
+    def getTextureSize(self):
+        return self.size
+    
+    def getTextureWidth(self):
+        return self.size[0]
+
+    def getTextureHeight(self):
+        return self.size[1]
+
     def getTextureAtLoc(self, loc):
         return self.atlas.subsurface(pygame.Rect((self.size[0]*loc, 0), self.size))
 
-    def getTexture(self, asset_name):
+    def getTextureLoc(self, asset_name):
         try:
             loc = self.asset_list.index(asset_name)
-            return getTextureAtLoc(loc)
+            return loc
         except ValueError:
             return self.default
+
+    def getTexture(self, asset_name):
+        return self.getTexterAtLoc(self.getTextureLoc(asset_name))
 
     def drawTextureAtLoc(self, surface, pos, loc):
         surface.blit(self.getTextureAtLoc(loc), pos)
 
     def drawTexture(self, surface, pos, asset_name):
         surface.blit(self.getTexture(asset_name), pos)
+
+
+
+GAME_ASSETS_FOLDER = os.path.join("assets", "game")
+
+class TEXTURE_PRESETS:
+    class MAP_TILES:
+        FOLDER      = os.path.join(GAME_ASSETS_FOLDER, "tiles")
+        SIZE        = 32
+        EXT         = ".png"
+
