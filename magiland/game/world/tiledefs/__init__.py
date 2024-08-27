b1bc091e40ff8deb5a7709db564a836dd4ff74c7
UncoolestCool <danielmycao@gmail.com>
2024-08-27 01:15:19 -0400

    updated __init__ (new tiles)

---
+++ magiland/game/world/tiledefs/__init__.py
@@ -0,0 +1,19 @@
+from .basic import BasicTile
+from .grass import GrassTile
+from .barrier import BarrierTile
+
+
+OVERWORLD_TILES = []
+GrassTile().addTileToList(OVERWORLD_TILES)
+BarrierTile().addTileToList(OVERWORLD_TILES)
+BasicTile("bloodstone", "bloodstone").addTileToList(OVERWORLD_TILES)
+BasicTile("sand", "sand").addTileToList(OVERWORLD_TILES)
+BasicTile("kr1stal", "kr1stal").addTileToList(OVERWORLD_TILES)
+BasicTile("snow", "snow").addTileToList(OVERWORLD_TILES)
+BasicTile("swamp", "swamp").addTileToList(OVERWORLD_TILES)
+BasicTile("void",
+          "barrier").addTileToList(OVERWORLD_TILES)
+
+
+ALL_TILES = []
+ALL_TILES += OVERWORLD_TILES
