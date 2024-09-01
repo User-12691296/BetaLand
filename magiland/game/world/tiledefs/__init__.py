54a6098d50f8eeb22b1981f288d05dd6bd5e406f
UncoolestCool <danielmycao@gmail.com>
2024-08-31 22:32:46 -0400

    Update __init__.py

--- magiland/game/world/tiledefs/__init__.py
+++ magiland/game/world/tiledefs/__init__.py
@@ -14,7 +14,7 @@ BasicTile("kr1stal", "kr1stal").addTileToList(OVERWORLD_TILES)
 BasicTile("snow", "snow").addTileToList(OVERWORLD_TILES)
 BasicTile("snow2", "snow2", False).addTileToList(OVERWORLD_TILES)
 BasicTile("swamp", "swamp").addTileToList(OVERWORLD_TILES)
-BasicTile("deepdark", "deepdark").addTileToList(OVERWORLD_TILES)
+BasicTile("deepdark", "deepdark", False).addTileToList(OVERWORLD_TILES)
 BasicTile("void",
           "barrier").addTileToList(OVERWORLD_TILES)
 
