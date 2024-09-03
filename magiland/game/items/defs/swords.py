f7d97b525385c273a9f7492da769eb13a6f30b3b
AnujN5038 <anuj.nay07@gmail.com>
2024-09-02 22:37:02 -0400

    Fixed recoil problem with help from my boi Seran :D

--- magiland/game/items/defs/swords.py
+++ magiland/game/items/defs/swords.py
@@ -11,7 +11,9 @@ class Sword(Item):
 
         self.damage = damage
         self.swing_angle = swing_angle
+        # recoil stuff
         self.recoil = recoil
+        self.recoil_once = False
         
         if not swing_range:
             self.swing_range = self.size + 2
@@ -44,6 +46,7 @@ class Sword(Item):
                     continue
                 
                 if entity in data["entities_hit"]:
+
                     continue
                     
                 angle_to = (180-round(math.degrees(math.atan2(player.pos[1]-entity.pos[1], player.pos[0]-entity.pos[0]))))%360
@@ -51,22 +54,22 @@ class Sword(Item):
                 if (angle_to >= data["rot"] - delta + 45) and (angle_to < data["rot"] + 45):
                     entity.damage(self.damage)
                     data["entities_hit"].append(entity)
+                    # this sets do to false so it only hits once and then do to true so never again (till startSwing)
+                    if self.recoil_once==False:
+                        player.damage(self.recoil)
+                        
+                        self.recoil_once=True
 
     def startSwing(self, data, player, world, tile_pos, tile):
         player.setMovable(False)
+        # this resets it every time type shi
+        self.recoil_once=False
         data["animations"].create("sword_swing", SWING_FRAMES, lambda: self.endSwing(data, player, world, tile_pos, tile))
 
         data["entities_hit"] = []
 
-        player.damage(self.recoil)
-
     def endSwing(self, data, player, world, tile_pos, tile):
         player.setMovable(True)
-
-        if data["entities_hit"]:
-            player.damage(0)
-        else:
-            player.damage(self.recoil * -1)
 
     def onLeft(self, data, player, world, tile_pos, tile):
         self.startSwing(data, player, world, tile_pos, tile)
