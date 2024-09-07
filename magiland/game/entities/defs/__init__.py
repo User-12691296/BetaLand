c0c5b663354d3f5f509b02dba8a0cfb5edaf7845
KavEn06 <kavinnimal@gmail.com>
2024-09-07 14:27:11 -0400

    Added Crystal Enemies

--- magiland/game/entities/defs/__init__.py
+++ magiland/game/entities/defs/__init__.py
@@ -1,6 +1,7 @@
 from .player import Player
 from .slimes import SLIMES, Slime
 from .blob import BLOBS, Blob
+from .crystalenemies import CRYSTALGOLEMS, CrystalGolem, CRYSTALKNIGHTS, CrystalKnight, CRYSTALSCORPIONS, CrystalScorpion, CRYSTALSIMES, CrystalSlime
 from .item import ItemEntity
 
 ALL_ENTITY_CLASSES = []
@@ -11,3 +12,9 @@ ALL_ENTITY_CLASSES.append(ItemEntity)
 
 ALL_ENTITY_CLASSES += SLIMES
 ALL_ENTITY_CLASSES += BLOBS
+
+# Add crystal enemies to a group of enemies
+ALL_ENTITY_CLASSES += CRYSTALGOLEMS
+ALL_ENTITY_CLASSES += CRYSTALKNIGHTS
+ALL_ENTITY_CLASSES += CRYSTALSCORPIONS
+ALL_ENTITY_CLASSES += CRYSTALSIMES
---
+++ magiland/game/entities/defs/crystalenemies.py
@@ -0,0 +1,111 @@
+from ..classes import Enemy
+
+class CrystalGolem(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["crystalgolem"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+
+        bpos = self.world.tilePosToBufferPos(self.pos)
+        spos = self.bufferPosToDisplayPos(bpos, display_topleft)
+        
+        self.atlas.drawTexture(display, spos, "crystalgolem")
+
+class CrystalKnight(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["crystalknight"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+
+        bpos = self.world.tilePosToBufferPos(self.pos)
+        spos = self.bufferPosToDisplayPos(bpos, display_topleft)
+        
+        self.atlas.drawTexture(display, spos, "crystalknight")
+
+class CrystalScorpion(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["crystalscorpion"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+
+        bpos = self.world.tilePosToBufferPos(self.pos)
+        spos = self.bufferPosToDisplayPos(bpos, display_topleft)
+        
+        self.atlas.drawTexture(display, spos, "crystalscorpion")
+
+class CrystalSlime(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["crystalslime"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+
+        bpos = self.world.tilePosToBufferPos(self.pos)
+        spos = self.bufferPosToDisplayPos(bpos, display_topleft)
+        
+        self.atlas.drawTexture(display, spos, "crystalslime")
+
+
+# Add crystal enemies to a group of enemies
+CRYSTALGOLEMS = []
+CrystalGolem.addToGroup(CRYSTALGOLEMS)
+
+CRYSTALKNIGHTS = []
+CrystalKnight.addToGroup(CRYSTALKNIGHTS)
+
+CRYSTALSCORPIONS = []
+CrystalScorpion.addToGroup(CRYSTALSCORPIONS)
+
+CRYSTALSIMES = []
+CrystalSlime.addToGroup(CRYSTALSIMES)
