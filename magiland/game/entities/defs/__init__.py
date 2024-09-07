c3df81ea92050e5c2641511aad4aa3c05d8a10d6
KavEn06 <kavinnimal@gmail.com>
2024-09-06 22:53:21 -0400

    added blob (anuj's code)

--- magiland/game/entities/defs/__init__.py
+++ magiland/game/entities/defs/__init__.py
@@ -1,5 +1,6 @@
 from .player import Player
 from .slimes import SLIMES, Slime
+from .blob import BLOBS, Blob
 from .item import ItemEntity
 
 ALL_ENTITY_CLASSES = []
@@ -9,3 +10,4 @@ ALL_ENTITY_CLASSES.append(Player)
 ALL_ENTITY_CLASSES.append(ItemEntity)
 
 ALL_ENTITY_CLASSES += SLIMES
+ALL_ENTITY_CLASSES += BLOBS
---
+++ magiland/game/entities/defs/blob.py
@@ -0,0 +1,56 @@
+"""  from ..classes import Enemy
+
+class Blob(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["blob"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, surface):
+        super().draw(surface)
+        self.atlas.drawTexture(surface, self.world.tilePosToBufferPos(self.pos), "blob")
+
+
+BLOBS = []
+Blob.addToGroup(BLOBS) """
+
+from ..classes import Enemy
+
+class Blob(Enemy):
+    def __init__(self):
+        super().__init__(5, 0, 30)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["blob"]
+
+    def isEnemy(self):
+        return True
+
+    def damageTick(self):
+        for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
+            if not entity.isEnemy():
+                entity.damage(0.1)
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+
+        bpos = self.world.tilePosToBufferPos(self.pos)
+        spos = self.bufferPosToDisplayPos(bpos, display_topleft)
+        
+        self.atlas.drawTexture(display, spos, "blob")
+
+
+# Add Blob to a group of enemies
+BLOBS = []
+Blob.addToGroup(BLOBS)
