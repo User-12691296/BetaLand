cb732e5ee303193359dba930cd6da2bfa09484d2
AnujN5038 <anuj.nay07@gmail.com>
2024-09-15 19:00:26 -0400

    Maces now spawn like 5 bullets. Have to update so it doesnt include the basic maces

--- magiland/game/items/defs/maces.py
+++ magiland/game/items/defs/maces.py
@@ -1,10 +1,11 @@
 from .swords import Sword
+from ...projectiles import PROJECTILE_CLASSES
 from misc import animations
 import math
 
 SWING_FRAMES = 10
 
-class Mace(Sword):pass
+class Mace(Sword):
 
 ##        #the only problem here is if you spam then it kills you lol
 ##    def startSwing(self, data, player, world, tile_pos, tile):
@@ -26,10 +27,24 @@ class Mace(Sword):pass
 ##            player.damage(0)
 ##        else:
 ##            player.damage(-1)
-
- 
-
-
+    def onLeft(self, data, player, world, tile_pos, tile):
+        super().onLeft(data,player,world,tile_pos,tile)
+        laser1 = PROJECTILE_CLASSES.CrystalLaserShot(player.pos, -data["rot"]-45)
+        laser1.giveImmunity(player)
+        world.addProjectile(laser1)
+        laser2 = PROJECTILE_CLASSES.CrystalLaserShot(player.pos, -data["rot"]-90)
+        laser2.giveImmunity(player)
+        world.addProjectile(laser2)
+        laser3 = PROJECTILE_CLASSES.CrystalLaserShot(player.pos, -data["rot"]-135)
+        laser3.giveImmunity(player)
+        world.addProjectile(laser3)            
+        laser4 = PROJECTILE_CLASSES.CrystalLaserShot(player.pos, -data["rot"]-180)
+        laser4.giveImmunity(player)
+        world.addProjectile(laser4)
+        laser5 = PROJECTILE_CLASSES.CrystalLaserShot(player.pos, -data["rot"]-225)
+        laser5.giveImmunity(player)
+        world.addProjectile(laser5)
+                                    
 MACES = []
 Mace("debug_sword", "sword", 1000, 2).addToGroup(MACES)
 Mace("wood_mace", "wood_mace", 20, 0, 300, 5).addToGroup(MACES)
