1d17791d5feedd046cd80ae8b9e57e11f96a0f94
AnujN5038 <94016622+AnujN5038@users.noreply.github.com>
2024-09-19 22:49:58 -0400

    Update __init__.py

--- magiland/game/projectiles/defs/__init__.py
+++ magiland/game/projectiles/defs/__init__.py
@@ -1,11 +1,11 @@
 from .lasershots import *
-from .pizza import *
+from .playerbullets import *
 from .enemybullets import *
 
 
 ALL_PROJECTILE_CLASSES = []
 
 ALL_PROJECTILE_CLASSES += LASERSHOTS
-ALL_PROJECTILE_CLASSES += PIZZA
+ALL_PROJECTILE_CLASSES += PLAYERBULLETS
 ALL_PROJECTILE_CLASSES += ENEMYBULLETS
 
