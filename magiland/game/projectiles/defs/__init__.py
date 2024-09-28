fa4251bca950676b8ecd2206eecb6c3d4e989ef8
AnujN5038 <anuj.nay07@gmail.com>
2024-09-27 22:10:37 -0400

    Added mace projectiles

--- magiland/game/projectiles/defs/__init__.py
+++ magiland/game/projectiles/defs/__init__.py
@@ -2,6 +2,7 @@ from .lasershots import *
 from .playerbullets import *
 from .enemybullets import *
 from .playerarrows import *
+from .maceprojectiles import *
 
 
 ALL_PROJECTILE_CLASSES = []
@@ -10,4 +11,5 @@ ALL_PROJECTILE_CLASSES += LASERSHOTS
 ALL_PROJECTILE_CLASSES += PLAYERBULLETS
 ALL_PROJECTILE_CLASSES += ENEMYBULLETS
 ALL_PROJECTILE_CLASSES += PLAYERARROWS
+ALL_PROJECTILE_CLASSES += MACEPROJECTILES
 
