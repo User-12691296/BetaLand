cc87e2b24e849733f2da57dfbfc52bd550ba7ce2
AnujN5038 <anuj.nay07@gmail.com>
2024-10-13 13:20:30 -0400

    Added bullets

--- magiland/game/projectiles/defs/playerbullets.py
+++ magiland/game/projectiles/defs/playerbullets.py
@@ -65,6 +65,141 @@ class SoulBlast(Projectile):
         super().draw(display, display_topleft)
         self.stdDraw(display, "soul_blast", display_topleft)
 
+class ThunderSpark(Projectile):
+    def __init__(self, start, angle):
+        super().__init__(start, angle)
+
+        self.speed = 0.3
+
+    @classmethod
+    def fromStartEnd(cls, start, end):
+        return super().fromStartEnd(start, end)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["thunderspark"]
+
+    def movementTick(self):
+        super().movementTick()
+
+        if self.world.isTileOpaque(self.getTilePos()):
+            self.kill()
+
+    def damageTick(self):
+        tpos = self.getTilePos()
+
+        for entity in self.world.getEntitiesOnTile(tpos):
+            if self.isValidHit(entity):
+                entity.damage(2)
+                self.kill()
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+        self.stdDraw(display, "thunderspark", display_topleft)
+
+class FrogBullet(Projectile):
+    def __init__(self, start, angle):
+        super().__init__(start, angle)
+
+        self.speed = 0.3
+
+    @classmethod
+    def fromStartEnd(cls, start, end):
+        return super().fromStartEnd(start, end)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["frog_bullet"]
+
+    def movementTick(self):
+        super().movementTick()
+
+        if self.world.isTileOpaque(self.getTilePos()):
+            self.kill()
+
+    def damageTick(self):
+        tpos = self.getTilePos()
+
+        for entity in self.world.getEntitiesOnTile(tpos):
+            if self.isValidHit(entity):
+                entity.damage(2)
+                self.kill()
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+        self.stdDraw(display, "frog_bullet", display_topleft)
+
+class ColdDoug(Projectile):
+    def __init__(self, start, angle):
+        super().__init__(start, angle)
+
+        self.speed = 0.3
+
+    @classmethod
+    def fromStartEnd(cls, start, end):
+        return super().fromStartEnd(start, end)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["cold_doug"]
+
+    def movementTick(self):
+        super().movementTick()
+
+        if self.world.isTileOpaque(self.getTilePos()):
+            self.kill()
+
+    def damageTick(self):
+        tpos = self.getTilePos()
+
+        for entity in self.world.getEntitiesOnTile(tpos):
+            if self.isValidHit(entity):
+                entity.damage(2)
+                self.kill()
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+        self.stdDraw(display, "cold_doug", display_topleft)
+        
+class PoisonDart(Projectile):
+    def __init__(self, start, angle):
+        super().__init__(start, angle)
+
+        self.speed = 0.3
+
+    @classmethod
+    def fromStartEnd(cls, start, end):
+        return super().fromStartEnd(start, end)
+
+    @staticmethod
+    def getNeededAssets():
+        return ["poison_dart"]
+
+    def movementTick(self):
+        super().movementTick()
+
+        if self.world.isTileOpaque(self.getTilePos()):
+            self.kill()
+
+    def damageTick(self):
+        tpos = self.getTilePos()
+
+        for entity in self.world.getEntitiesOnTile(tpos):
+            if self.isValidHit(entity):
+                entity.damage(2)
+                self.kill()
+
+    def draw(self, display, display_topleft=(0, 0)):
+        super().draw(display, display_topleft)
+        self.stdDraw(display, "poison_dart", display_topleft) 
+
 PLAYERBULLETS = []
 Pizza.addToGroup(PLAYERBULLETS)
 SoulBlast.addToGroup(PLAYERBULLETS)
+ThunderSpark.addToGroup(PLAYERBULLETS)
+FrogBullet.addToGroup(PLAYERBULLETS)
+PoisonDart.addToGroup(PLAYERBULLETS)
+ColdDoug.addToGroup(PLAYERBULLETS)
+FrogBullet.addToGroup(PLAYERBULLETS)
+                                
+
