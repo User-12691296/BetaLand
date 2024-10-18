dd64a8387814889a2306e7ed861cd2c9248f6cb6
UncoolestCool <danielmycao@gmail.com>
2024-10-18 10:04:36 -0400

    death code

--- magiland/game/world/tiledefs/grass.py
+++ magiland/game/world/tiledefs/grass.py
@@ -3,7 +3,3 @@ from .basic import BasicTile
 class GrassTile(BasicTile):
     def __init__(self):
         super().__init__("grass", "grass",False)
-
-    def onLeft(self, world, tile_pos):
-        world.setTileElevation(tile_pos, 10)
-        world.setTileID(tile_pos, "barrier")
