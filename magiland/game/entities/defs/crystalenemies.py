1d3dc6bc6f3969ea6ba0ec98e7baf943faffa086
AnujN5038 <94016622+AnujN5038@users.noreply.github.com>
2024-10-18 16:33:38 -0400

    Update crystalenemies.py

--- magiland/game/entities/defs/crystalenemies.py
+++ magiland/game/entities/defs/crystalenemies.py
@@ -4,7 +4,7 @@ from ..classes import Enemy
 
 class CrystalGolem(Enemy):
     def __init__(self):
-        super().__init__(5, 0, 40)
+        super().__init__(3, 0, 40)
         """ CODE FOR MOB DROPS
         self.loadInventory() """
 
@@ -18,7 +18,7 @@ class CrystalGolem(Enemy):
     def damageTick(self):
         for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
             if not entity.isEnemy():
-                entity.damage(0.2)
+                entity.damage(0.4)
 
     """ CODE FOR MOB DROPS
     def tick(self):
@@ -39,7 +39,7 @@ class CrystalGolem(Enemy):
 
 class CrystalKnight(Enemy):
     def __init__(self):
-        super().__init__(5, 0, 25)
+        super().__init__(5, 0, 20)
 
     @staticmethod
     def getNeededAssets():
@@ -66,7 +66,7 @@ class CrystalKnight(Enemy):
 
 class CrystalScorpion(Enemy):
     def __init__(self):
-        super().__init__(5, 0, 25)
+        super().__init__(8, 0, 25)
 
     @staticmethod
     def getNeededAssets():
@@ -78,7 +78,7 @@ class CrystalScorpion(Enemy):
     def damageTick(self):
         for entity in self.world.getEntitiesInRangeOfTile(self.pos, 1.5):
             if not entity.isEnemy():
-                entity.damage(0.2)
+                entity.damage(0.1)
 
     def draw(self, display, display_topleft=(0, 0)):
         bpos = self.world.tilePosToBufferPos(self.pos)
@@ -93,7 +93,7 @@ class CrystalScorpion(Enemy):
 
 class CrystalSlime(Enemy):
     def __init__(self):
-        super().__init__(5, 0, 1)
+        super().__init__(4, 0, 1)
 
     @staticmethod
     def getNeededAssets():
