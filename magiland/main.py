c83a36736dd95d62c5b9dcd1b0535a1c4ae022f3
UncoolestCool <danielmycao@gmail.com>
2024-10-18 01:26:13 -0400

    fixed serans boss code, added retextured death void, added send to spawn code

---
+++ magiland/main.py
@@ -0,0 +1,45 @@
+import pygame
+pygame.init()
+
+from screen import Screen
+# Create the screen
+ASPECT_RATIO = 8/5
+RESOLUTION = 1600
+SCREEN = Screen(ASPECT_RATIO, RESOLUTION)
+
+from managers import MainWindowManager
+from misc import events
+
+clean_close = False
+
+# Reroutes events and drawing commands through a manager to clean the main code
+main_manager = MainWindowManager(SCREEN)
+
+# Main loop
+clock = pygame.time.Clock()
+running = True
+while running:
+    # Tick before input 
+    main_manager.first_tick()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        main_manager.handleEvent(event)
+
+    # Tick after input
+    main_manager.main_tick()
+
+    # Draw to screen
+    SCREEN.get().fill((0, 0, 0))
+
+    main_manager.draw(SCREEN.get())
+    
+    SCREEN.update()
+
+    # FPS control
+    clock.tick(60)
+
+pygame.quit()
+
