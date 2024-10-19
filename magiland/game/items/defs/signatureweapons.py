11a9221326b6c13e93785dfe1fe098164206ba18
AnujN5038 <94016622+AnujN5038@users.noreply.github.com>
2024-10-18 20:07:19 -0400

    Update signatureweapons.py

--- magiland/game/items/defs/signatureweapons.py
+++ magiland/game/items/defs/signatureweapons.py
@@ -32,4 +32,4 @@ class SigWeapon(Sword):
 SIGWEAPONS = []
 SigWeapon("clawofcrabking", "clawofcrabking", 35, 2, 360, laser=PROJECTILE_CLASSES.CelestialMaceProj, cooldown=20).addToGroup(SIGWEAPONS)
 SigWeapon("clawofsnakequeen", "clawnofsnakequeen", 35, 2, 360, laser=PROJECTILE_CLASSES.PoisonDart, cooldown=20).addToGroup(SIGWEAPONS)
-SigWeapon("scissors", "scissors", 35,2,360,laser=PROJECTILE_CLASSES.IceBullet,cooldown=20).addToGroup(SIGWEAPONS)
+SigWeapon("scissors", "scissors", 35,2,360,laser=PROJECTILE_CLASSES.ScissorBullet,cooldown=20).addToGroup(SIGWEAPONS)
